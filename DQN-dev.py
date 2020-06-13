2177cc7ba7aa224b4c7ceadc5b3d835642abf661
Fred Bruford <fred.bruford@gmail.com>
2020-06-13 21:41:43 +0100

    initializing weights with LSTM output. generated loops not correct

--- DQN-dev.py
+++ DQN-dev.py
@@ -1,15 +1,17 @@
 import numpy as np
 import random
 from tensorflow.keras.models import load_model
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import Adam
 
 from collections import deque
 
+np.set_printoptions(threshold=np.inf,precision=2)
 
 class DQN:
-    def __init__(self):
+    def __init__(self, LSTM, seedLoop):
         self.memory = deque(maxlen=2000)
 
         self.gamma = 0.85
@@ -19,20 +21,25 @@ class DQN:
         self.learning_rate = 0.005
         self.tau = .125
 
+        self.probabilities = LSTM.predict(np.expand_dims(seedLoop, 0))[0]
+        print("LSTM prediction = ", convertOneHotToList(self.probabilities))
         self.model = self.create_model()
         self.target_model = self.create_model()
 
     def create_model(self):
         model = Sequential()
         #state_shape = [16,32]
-        model.add(Dense(32, input_shape=(16,32), activation="relu")) # does this need to be 512? size of state space? flatten model?
+        model.add(Dense(16, input_shape=(32,)))
+        model.add(Dense(24, input_shape=(16,32), activation="relu")) # does this need to be 512? size of state space? flatten model?
         model.add(Dense(48, activation="relu"))
         model.add(Dense(24, activation="relu"))
         model.add(Dense(32)) #number of possible actions
         model.compile(loss="mean_squared_error",
                       optimizer=Adam(lr=self.learning_rate))
         model.summary()
-
+        weights = model.get_weights()
+        weights[0] = self.probabilities.T
+        model.set_weights(weights)
         return model
 
     def act(self, state):
@@ -44,7 +51,7 @@ class DQN:
             # actions are adding or removing any number of onsets
             action, actionIndex = self.getRandomAction()  # pick random action
         else:
-            prediction = self.model.predict(np.expand_dims(state, 0))[0,:,:]
+            prediction = self.model.predict(state)
             actionIndex = np.unravel_index(np.argmax(prediction, axis=None), prediction.shape) # do predicted best action. add batch size
             action = np.zeros([16,32])
             action[actionIndex] = 1
@@ -96,7 +103,7 @@ class DQN:
     def replay(self): # need to refit this function to use new adapted reward calculation?
         # this is not training the target -  this is training self.model, using target estimated by
         # by target model
-        batch_size = 512
+        batch_size = 1024
         if len(self.memory) < batch_size:
             return
 
@@ -104,17 +111,16 @@ class DQN:
 
         for sample in samples:
             currentState, action, reward, newState, done = sample
-            target = self.target_model.predict(np.expand_dims(currentState, 0))
+            target = self.target_model.predict(currentState)
             # print(target[0].shape, 'shape') # target is an action. target[0] = 16x32
-            y,z  = np.unravel_index(np.argmax(target[0], axis=None), target[0].shape)
             if done:
                 target[0] = reward
             else:
-                Q_future = max(self.target_model.predict(np.expand_dims(newState, 0)))
+                Q_future = np.max(self.target_model.predict(newState))
                 # print(Q_future)
                 target[0] = reward + Q_future * self.gamma
             # print(target.shape)
-            self.model.fit(np.expand_dims(currentState, 0), target, epochs=1, verbose=0) #fit updates model weights
+            self.model.fit(currentState, target, epochs=1, verbose=0) #fit updates model weights
 
     def target_train(self):
         weights = self.model.get_weights()
@@ -126,8 +132,8 @@ class DQN:
     def save_model(self, fn):
         self.model.save(fn)
 
-    def initWeights(self):
-        return
+    def initWeights(self,shape):
+        return self.probabilities
 
 def convertOneHotToList(groove):
     # convert one hot or probabilities matrix from LSTM model into a list format
@@ -139,6 +145,7 @@ def convertOneHotToList(groove):
         index = np.argmax(groove[i, :])
         grooveList.append(columns[index])
     return grooveList
+
 
 gamma = 0.9
 epsilon = .95
@@ -153,17 +160,19 @@ LSTM = load_model("JAKI_Encoder_Decoder_12-6-20")
 
 
 # updateTargetNetwork = 1000
-dqn_agent = DQN()
+
 steps = []
 for trial in range(trials):
     # reset environment to a random loop
-    groove = oneBarGrooves[np.random.randint(0,6000),:,:]
+    seedLoop = oneBarGrooves[np.random.randint(0,6000),:,:]
+    dqn_agent = DQN(LSTM, seedLoop)
+
     # print(LSTM.summary())
     # LSTMProbability = LSTM.predict(np.expand_dims(groove, 0))[0]
     # print('\n')
     # print(dqn_agent.model.layers[0].get_weights()[1].shape)
     # dqn_agent.model.layers[0].set_weights(LSTMProbability)
-    currentState = groove
+    currentState = seedLoop
 
     for step in range(trial_len):
         newState, reward, done, action = dqn_agent.act(currentState) #todo: do the action
@@ -174,7 +183,7 @@ for trial in range(trials):
 
         currentState = newState
         if done:
-            print("1st", convertOneHotToList(groove))
+            print("1st", convertOneHotToList(seedLoop))
             print("Gen", convertOneHotToList(currentState))
             break
     if step >= 1499:
