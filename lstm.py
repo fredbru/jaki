4cdc93c4e2203888c9d39d7da08c856d5b0f36f9
Fred Bruford <fred.bruford@gmail.com>
2020-08-03 17:07:48 +0100

    refactor

--- lstm.py
+++ lstm.py
@@ -11,12 +11,12 @@ oneHotPatterns = np.load("one_hot_drum_loops.npy")
 np.random.shuffle(oneHotPatterns)
 
 print("Number of Patterns = ", oneHotPatterns.shape[0])
+trainTestSplit = oneHotPatterns.shape[0]-400
+bar1 = oneHotPatterns[0:trainTestSplit,0:16,:]
+bar2 = oneHotPatterns[0:trainTestSplit,16:32,:]
 
-bar1 = oneHotPatterns[0:5000,0:16,:]
-bar2 = oneHotPatterns[0:5000,16:32,:]
-
-bar1Validate = oneHotPatterns[5000:oneHotPatterns.shape[0],0:16,:]
-bar2Validate = oneHotPatterns[5000:oneHotPatterns.shape[0],16:32,:]
+bar1Validate = oneHotPatterns[trainTestSplit:oneHotPatterns.shape[0],0:16,:]
+bar2Validate = oneHotPatterns[trainTestSplit:oneHotPatterns.shape[0],16:32,:]
 
 X_train = bar1
 y_train = bar2
@@ -27,15 +27,10 @@ y_valid = bar2Validate
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.LSTM(250, input_shape=(16,32),return_sequences=True)) #encoder
 model.add(tf.keras.layers.LSTM(100, input_shape=(16,32),dropout=0.2,recurrent_dropout=0.2)) #encoder
-model.summary()
-
 model.add(tf.keras.layers.RepeatVector(16))
 model.add(tf.keras.layers.LSTM(16, return_sequences=True, input_shape=(100,))) #decoder
 model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(32, activation='softmax')))
-
-model.compile(
-     loss='mse', metrics=['accuracy'])
-
+model.compile(loss='mse', metrics=['accuracy'])
 model.summary()
 
 callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=40)]
@@ -46,4 +41,4 @@ history = model.fit(X_train,  y_train,
                     callbacks=callbacks,
                     validation_data=(bar1Validate,bar2Validate))
 
-model.save("JAKI_Encoder_Decoder_3-8_1")
+model.save("JAKI_Encoder_Decoder_Trained")
